bedc0ca69fb99b4c41030bb2c77fc9d8ea9451cf
xtlsoft <xtlsoft@126.com>
2018-01-01 16:01:37 +0800

    [+] FInished Client.

--- src/Client.php
+++ src/Client.php
@@ -16,6 +16,95 @@
 
     class Client {
 
+        protected $server;
+        protected $transport = "tcp";
+        protected $username = "guest";
+        protected $password = "guest";
+        protected $map = [];
 
+        public function __construct($server){
+
+            $this->server = "ws://$server";
+
+        }
+
+        public function enableSSL($bool = true){
+
+            if($bool) $this->transport = "ssl";
+            else $this->transport = "tcp";
+
+            return $this;
+
+        }
+
+        public function login($user, $pass){
+
+            $this->username = $user;
+            $this->password = $pass;
+
+            return $this;
+
+        }
+
+        public function map($orgin, $mirror, $trans = "tcp"){
+
+            $atc = "";
+            $server = $this->server;
+
+            $fpkg = $this->username ."||" . $this->password . "||" . $orgin . "||" . $trans;
+
+            $worker = new \Workerman\Worker($mirror);
+
+            $worker->onConnect = function ($conn) use ($fpkg, &$atc, $server) {
+                $atc = new \Workerman\Connection\AsyncTcpConnection($server);
+
+                $atc->onMessage = function($c, $m) use ($conn){
+                    $conn->send(base64_decode($m));
+                };
+                $atc->onClose = function($c) use ($conn){
+                    $conn->close();
+                };
+
+                $atc->connect();
+                $atc->send($fpkg);
+            };
+
+            $worker->onMessage = function($conn, $msg) use ($atc){
+                $atc->send(base64_encode($msg));
+            };
+
+            $worker->onClose = function($conn) use ($atc){
+                $atc->close();
+            };
+
+            $this->map[] = [$orgin, $mirror, $trans];
+
+            return $this;
+
+        }
+
+        public function importMap($map){
+
+            if(!is_array($map)){
+                $map = \json_decode(file_get_contents($map), 1);
+            }
+
+            foreach($map as $v) {
+
+                $this->map($v[0], $v[1], $v[2]);
+
+            }
+
+            return $this;
+
+        }
+
+        public function exportMap($map){
+
+            \file_put_contents($map, \json_encode($this->map));
+
+            return $this;
+
+        }
         
     }
--- testc.php
+++ testc.php
@@ -4,14 +4,13 @@ require "vendor/autoload.php";
 
 $client = new \CloudSky\OnePort\Client("127.0.0.1:5280");
 
-$client->connect(["ssl" => true]);
+$client->enableSSL(true);
 
 $client->login("user1", "123456");
 
 $client->importMap("map.json");
 
 $client->map("tcp://10.0.50.23:5080", "tcp://127.0.0.23:5080");
-$client->map("2345", "22345");
 
 $client->map("someservice", "22346");
 
